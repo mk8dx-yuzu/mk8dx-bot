0d9f6fac3b685434f072c47926657d3793b927e8
probablyjassin <aouanijassin@gmail.com>
2024-02-23 18:21:46 +0100

    player stats embed

--- cogs/mk8dx.py
+++ cogs/mk8dx.py
@@ -38,9 +38,9 @@ class mk8dx(commands.Cog):
     @slash_command(name="mmr", description="Retrieve the MMR of a player")
     @commands.cooldown(2, 120, commands.BucketType.user)
     async def mmr(self, ctx: discord.ApplicationContext, name: str):
-        player = self.collection.find_one({"name": name})
-        if player:
-            await ctx.respond(f"{name}s MMR is {player['mmr']}")
+        mmr = self.collection.find_one({"name": "probablyjassin"}, {"_id": 0, "mmr": 1}).get("mmr")
+        if mmr:
+            await ctx.respond(f"{name}s MMR is {mmr}")
         else:
             await ctx.respond(f"Couldn't find {name}s MMR")
 
@@ -83,11 +83,11 @@ class mk8dx(commands.Cog):
 
         table_string = ""
         table_string += "```\n"
-        table_string += " | Name            |   Rank   |  MMR  | Wins | Losses | Winrate (%) |\n"
-        table_string += " |-----------------|----------|-------|------|--------|-------------|\n"
+        table_string += " |  #  | Name            |   Rank   |  MMR  | Wins | Losses | Winrate (%) |\n"
+        table_string += " |-----|-----------------|----------|-------|------|--------|-------------|\n"
         for player in data[start_index:end_index]:
             games = player['wins']+player['losses']
-            table_string += f" | {player['name']:<15} | {calcRank(player['mmr']):>8} | {player['mmr']:>5} | {player['wins']:>4} | {player['losses']:>6} | {(player['wins']/games if games else 0)*100:>11} |\n"
+            table_string += f" | {data.index(player)+1:<3} | {player['name']:<15} | {calcRank(player['mmr']):>8} | {player['mmr']:>5} | {player['wins']:>4} | {player['losses']:>6} | {(player['wins']/games if games else 0)*100:>11} |\n"
         table_string += f"Page {page}"
         table_string += "```"
 
@@ -103,29 +103,67 @@ class mk8dx(commands.Cog):
         player: dict = self.collection.find_one({"name": name})
         if not player:
             return await ctx.respond("Couldn't find that player")
-        player_str = f"## {name} \n"
-        player_str += "```\n"
+
+        embed = discord.Embed(
+            title=f"{name}",
+            description="",
+            color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+        )
         for item in list(player.keys())[2:]:
-            player_str += f"{item}: {player[item]} \n"
-        games = (player['wins']+player['losses'])
-        player_str += f"Winrate: {(player['wins']/games if games else 0)*100}% \n"
-        player_str += "```"
-        await ctx.respond(player_str)
+            embed.add_field(name=f"{item}", value=f"{player[item]}")
+        
+        embed.add_field(name="Rank", value=f"{calcRank(player['mmr'])}")
+        embed.add_field(name="Winrate", value=f"{(player['wins']/(player['wins']+player['losses']) if (player['wins']+player['losses']) else 0)*100}%")
+    
+        embed.set_author(name="Yuzu-Lounge", icon_url="https://mario.wiki.gallery/images/4/40/MKT_Icon_Triple_Mushrooms.png")
+        embed.set_thumbnail(url="https://mario.wiki.gallery/images/4/40/MKT_Icon_Triple_Mushrooms.png")
+
+        await ctx.respond(f"# {name} - overview", embed=embed)
 
     @slash_command(name="register", description="Register for playing in the Lounge")
-    async def register(interaction: discord.Interaction, ctx: discord.ApplicationContext):
+    async def register(
+            self,
+            ctx: discord.ApplicationContext,
+            username = Option(
+                str, 
+                description="Your username to show up on the leaderboard",
+                required=True,
+            )
+        ):
         role = ctx.guild.get_role(1181313896695480321)
-        print(role)
-        print("ok")
         member = ctx.user
         if role in member.roles:
             await ctx.respond("You already have the Lounge Player role")
             return
-        print("wow")
         await member.add_roles(role)
-        await ctx.respond(f"{member.name} is now registered for Lounge")
-        print("dones")
+        self.collection.insert_one({
+            "name": username,
+            "mmr": 2000,
+            "wins": 0,
+            "losses": 0
+        })
+        await ctx.respond(f"{member.name} is now registered for Lounge as {username}")
 
+    @slash_command(name="test")
+    async def test(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="My Amazing Embed",
+            description="Embeds are super easy, barely an inconvenience.",
+            color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+        )
+        embed.add_field(name="A Normal Field", value="A really nice field with some information. **The description as well as the fields support markdown!**")
+        embed.add_field(name="A Normal Field", value="A really nice field with some information. **The description as well as the fields support markdown!**")
+
+        embed.add_field(name="Inline Field 1", value="Inline Field 1", inline=True)
+        embed.add_field(name="Inline Field 2", value="Inline Field 2", inline=True)
+        embed.add_field(name="Inline Field 3", value="Inline Field 3", inline=True)
+    
+        embed.set_footer(text="Footer! No markdown here.") # footers can have icons too
+        #embed.set_author(name="Pycord Team", icon_url="https://mario.wiki.gallery/images/4/40/MKT_Icon_Triple_Mushrooms.png") # tiny top
+        embed.set_thumbnail(url="https://mario.wiki.gallery/images/4/40/MKT_Icon_Triple_Mushrooms.png") # big top right
+        #embed.set_image(url="https://mario.wiki.gallery/images/4/40/MKT_Icon_Triple_Mushrooms.png") # huge bottom
+    
+        await ctx.respond("Hello! Here's a cool embed.", embed=embed) # Send the embed with some text
 
 def setup(bot: commands.Bot):
     bot.add_cog(mk8dx(bot))
--- test.py
+++ test.py
@@ -16,4 +16,8 @@ docs = collection.find()
 for doc in docs:
     print(doc)
 
+player_mmr = collection.find_one({"name": "probablyjassin"}, {"_id": 0, "mmr": 1})
+print(player_mmr)
+
+
 client.close()
