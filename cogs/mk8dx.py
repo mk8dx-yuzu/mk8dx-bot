a06efa48431f8ce7ac9f4e5663a0b539adda5650
probablyjassin <aouanijassin@gmail.com>
2024-04-06 16:12:36 +0200

    change rank threshholds (infinity)

--- cogs/mk8dx.py
+++ cogs/mk8dx.py
@@ -8,13 +8,13 @@ from discord import slash_command, Option
 
 def calcRank(mmr):
     ranks = [
-        {"name": "Wood", "range": (-999999999, 0)},
+        {"name": "Wood", "range": (-math.inf, 0)},
         {"name": "Bronze", "range": (0, 1499)},
         {"name": "Silver", "range": (1400, 2999)},
         {"name": "Gold", "range": (3000, 5099)},
         {"name": "Platinum", "range": (5100, 6999)},
         {"name": "Diamond", "range": (7000, 9499)},
-        {"name": "Master", "range": (9500, 99999)},
+        {"name": "Master", "range": (9500, math.inf)},
     ]
     for range_info in ranks:
         start, end = range_info["range"]
