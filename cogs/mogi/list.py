411d957da82088b6c79b8d8e636d8609c2aa4a08
probablyjassin <aouanijassin@gmail.com>
2024-05-09 14:00:38 +0200

    put /l and /teams in cog

---
+++ cogs/mogi/list.py
@@ -0,0 +1,64 @@
+import discord
+from discord import Option, ApplicationContext, slash_command
+from discord.ext import commands
+from discord.utils import get
+
+class list(commands.Cog):
+    def __init__(self, bot):
+            self.bot: commands.Bot = bot
+
+    @slash_command(name="l", description="List all players in the current mogi")
+    async def l(self, ctx: ApplicationContext, 
+                table = Option(
+                    name="table", 
+                    description="Omit numbers to copy and paste into a table maker",
+                    required=False,
+                    choices = ["y"]
+                    )):
+        if not self.bot.mogi["status"]:
+            return await ctx.respond("Currently no open mogi")
+        if not self.bot.mogi["players"]:
+            return await ctx.respond("Current mogi: \n No players")
+        list = "Current mogi:\n"
+        for index, player in enumerate(self.bot.mogi["players"]):
+            try:
+                if get(ctx.guild.members, id=int(player.strip("<@!>"))).nick:
+                    name = get(ctx.guild.members, id=int(player.strip("<@!>"))).nick
+                else:
+                    try:
+                        name = get(ctx.guild.members, id=int(player.strip("<@!>"))).display_name
+                    except:
+                        name = player
+            except:
+                try:
+                    name = get(ctx.guild.members, id=int(player.strip("<@!>"))).display_name
+                except:
+                    name = player
+            if table:
+                list += f"{name} +\n\n"
+            else:
+                list += f"*{index+1}.* {name}\n"
+        await ctx.respond(list, allowed_mentions=discord.AllowedMentions(users=False))
+
+    @slash_command(name="teams", description="Show teams")
+    async def teams(self, ctx: ApplicationContext):
+        if not self.bot.mogi["status"]:
+            return await ctx.respond("No open mogi", ephemeral=True)
+        if not len(self.bot.mogi["teams"]):
+            return await ctx.respond("No teams decided yet", ephemeral=True)
+        lineup_str = "# Teams \n\n"
+        if self.bot.mogi["format"] == "ffa":
+            for i, player in enumerate(self.bot.mogi["players"]):
+                lineup_str += f"`{i+1}:` {get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name}\n"
+        else:
+            for i, team in enumerate(self.bot.mogi["teams"]):
+                lineup_str += f"{self.bot.mogi['team_tags'][i]}\n"
+                team = [get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name for player in team]
+                for player in team:   
+                    lineup_str += f"{player} +\n"
+                lineup_str += "\n"
+                    
+        await ctx.respond(lineup_str)
+        
+def setup(bot: commands.Bot):
+    bot.add_cog(list(bot))
--- cogs/mogi/mogi.py
+++ cogs/mogi/mogi.py
@@ -35,38 +35,7 @@ class mogi(commands.Cog):
         await ctx.respond(f"New mogi locking state: {self.bot.mogi['locked']}")
         
 
-    @slash_command(name="l", description="List all players in the current mogi")
-    async def l(self, ctx: ApplicationContext, 
-                table = Option(
-                    name="table", 
-                    description="Omit numbers to copy and paste into a table maker",
-                    required=False,
-                    choices = ["y"]
-                    )):
-        if not self.bot.mogi["status"]:
-            return await ctx.respond("Currently no open mogi")
-        if not self.bot.mogi["players"]:
-            return await ctx.respond("Current mogi: \n No players")
-        list = "Current mogi:\n"
-        for index, player in enumerate(self.bot.mogi["players"]):
-            try:
-                if get(ctx.guild.members, id=int(player.strip("<@!>"))).nick:
-                    name = get(ctx.guild.members, id=int(player.strip("<@!>"))).nick
-                else:
-                    try:
-                        name = get(ctx.guild.members, id=int(player.strip("<@!>"))).display_name
-                    except:
-                        name = player
-            except:
-                try:
-                    name = get(ctx.guild.members, id=int(player.strip("<@!>"))).display_name
-                except:
-                    name = player
-            if table:
-                list += f"{name} +\n\n"
-            else:
-                list += f"*{index+1}.* {name}\n"
-        await ctx.respond(list, allowed_mentions=discord.AllowedMentions(users=False))
+    
 
     @slash_command(
         name="start", description="Randomize teams, vote format and start playing", guild_only=True
@@ -241,25 +210,7 @@ class mogi(commands.Cog):
         await ctx.respond("The mogi has been stopped, use /start to start it again")
         await ctx.send(f"Debug:\nVotes:{self.bot.mogi['votes']}")
 
-    @slash_command(name="teams", description="Show teams")
-    async def teams(self, ctx: ApplicationContext):
-        if not self.bot.mogi["status"]:
-            return await ctx.respond("No open mogi", ephemeral=True)
-        if not len(self.bot.mogi["teams"]):
-            return await ctx.respond("No teams decided yet", ephemeral=True)
-        lineup_str = "# Teams \n\n"
-        if self.bot.mogi["format"] == "ffa":
-            for i, player in enumerate(self.bot.mogi["players"]):
-                lineup_str += f"`{i+1}:` {get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name}\n"
-        else:
-            for i, team in enumerate(self.bot.mogi["teams"]):
-                lineup_str += f"{self.bot.mogi['team_tags'][i]}\n"
-                team = [get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name for player in team]
-                for player in team:   
-                    lineup_str += f"{player} +\n"
-                lineup_str += "\n"
-                    
-        await ctx.respond(lineup_str)
+    
 
     
 
