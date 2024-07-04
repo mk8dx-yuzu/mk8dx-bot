74f9dbb695833c7ba27163cf9a4f7a63a8dddaa2
probablyjassin <aouanijassin@gmail.com>
2024-07-04 19:26:34 +0200

    fix the fix

--- cogs/roles.py
+++ cogs/roles.py
@@ -10,13 +10,12 @@ class roles(commands.Cog):
 
     @slash_command(name="give_s1_roles")
     async def give_s1_roles(self, ctx: ApplicationContext):
+        guild = self.bot.get_guild(1084911987626094654)
         data = []
         with open("./cogs/extras/season_1_end_leaderboard-2024-07-04.json") as file:
             data = json.loads(file.read())
         for player in data:
-            discord_member: discord.Member = get(ctx.guild.members, id=int(player["discord"]))
-            discord_member.add_roles(get(ctx.guild.roles, name="Season 1 Player"))
-    
+            guild.get_member(int(player["discord"])).add_roles(get(ctx.guild.roles, name="Season 1 Player"))
 
 def setup(bot: commands.Bot):
     bot.add_cog(roles(bot))
