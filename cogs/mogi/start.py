970eb11311fa12ae6f41f17f8f1635aab1c9cfae
probablyjassin <aouanijassin@gmail.com>
2024-07-06 17:06:06 +0200

    vote via buttons

--- cogs/mogi/start.py
+++ cogs/mogi/start.py
@@ -10,6 +10,53 @@ from discord.ext import commands
 from discord.utils import get
 from discord.ui import View
 
+
+def vote(bot: commands.Bot, format: str, user: discord.User):
+    bot.mogi["votes"][format] += 1
+    bot.mogi["voters"].append(user.mention)
+
+def canVote(bot: commands.Bot, format: str, user: discord.User.mention):
+    size = int(format[0]) if format[0] != "f" else 1
+    return (
+        (len(bot.mogi["players"]) % size == 0 and len(bot.mogi["players"]) > size)
+        and user in bot.mogi["players"]
+        and user not in bot.mogi["voters"]
+    )
+
+def isDecided(bot: commands.Bot):
+    return (len(bot.mogi["voters"]) >= len(bot.mogi["players"])) or (
+        bot.mogi["votes"][max(bot.mogi["votes"], key=bot.mogi["votes"].get)]
+        >= math.floor(len(bot.mogi["players"]) / 2) + 1
+    )
+
+def startMogi(bot: commands.Bot):
+    max_voted = max(bot.mogi["votes"], key=bot.mogi["votes"].get)
+    bot.mogi["format"] = max_voted
+    lineup_str = ""
+    if max_voted == "ffa":
+        for i, player in enumerate(bot.mogi["players"]):
+            lineup_str += f"`{i+1}:` {player}\n"
+            bot.mogi["teams"].append([player])
+    else:
+        random.shuffle(bot.mogi["players"])
+        teams = []
+        for i in range(0, len(bot.mogi["players"]), int(max_voted[0])):
+            teams.append(
+                bot.mogi["players"][i : i + int(max_voted[0])]
+            )
+        bot.mogi["teams"] = teams
+        for i, item in enumerate(teams):
+            lineup_str += f"\n `{i+1}`. {', '.join(item)}"
+
+    votes = "## Vote results:\n"
+    for item in bot.mogi["votes"].keys():
+        votes += f"{item}: {bot.mogi['votes'][item]}\n"
+    bot.mogi["votes"] = {key: 0 for key in bot.mogi["votes"]}
+    bot.mogi["voting"] = 0
+    bot.mogi["running"] = 1
+
+    return f"{votes}\n# Mogi starting!\n## Format: {max_voted}\n### Lineup:\n{lineup_str}"
+
 class Menu(discord.ui.View):
     def __init__(self, bot, count):
         super().__init__()
@@ -41,74 +88,58 @@ class Menu(discord.ui.View):
 
     @discord.ui.button(label="FFA", style=discord.ButtonStyle.blurple)
     async def btnffa(self, button: discord.ui.Button, interaction: Interaction):
-        if interaction.user.mention not in self.bot.mogi["players"]:
-            return await interaction.respond("Can't vote for that", ephemeral=True)
+        if not canVote(self.bot, button.label.lower(), interaction.user.mention):
+            return await interaction.respond("Can't vote on that", ephemeral=True)
 
-        if interaction.user.mention in self.bot.mogi["voters"]:
-            return await interaction.respond("You voted already", ephemeral=True)
-
-        self.bot.mogi["voters"].append(interaction.user.mention)
-        self.bot.mogi["votes"][button.label.lower()] += 1
+        vote(self.bot, button.label.lower(), interaction.user)
         await interaction.respond("Voted for FFA", ephemeral=True)
+
+        if isDecided(self.bot):
+            await interaction.channel.send(startMogi(self.bot))
 
     @discord.ui.button(label="2v2")
     async def btn2v2(self, button: discord.ui.Button, interaction: Interaction):
-        if (
-            not (self.count % 2 == 0 and self.count > 2)
-        ) or interaction.user.mention not in self.bot.mogi["players"]:
-            return await interaction.respond("Can't vote for that", ephemeral=True)
+        if not canVote(self.bot, button.label.lower(), interaction.user.mention):
+            return await interaction.respond("Can't vote on that", ephemeral=True)
 
-        if interaction.user.mention in self.bot.mogi["voters"]:
-            return await interaction.respond("You voted already", ephemeral=True)
-        
-        self.bot.mogi["voters"].append(interaction.user.mention)
-        self.bot.mogi["votes"][button.label.lower()] += 1
+        vote(self.bot, button.label.lower(), interaction.user)
         await interaction.respond("Voted for 2v2", ephemeral=True)
+
+        if isDecided(self.bot):
+            await interaction.channel.send(startMogi(self.bot))
 
     @discord.ui.button(label="3v3")
     async def btn3v3(self, button: discord.ui.Button, interaction: Interaction):
-        if (
-            not (self.count % 3 == 0 and self.count > 3)
-        ) or interaction.user.mention not in self.bot.mogi["players"]:
-            return await interaction.respond("Can't vote for that", ephemeral=True)
+        if not canVote(self.bot, button.label.lower(), interaction.user.mention):
+            return await interaction.respond("Can't vote on that", ephemeral=True)
 
-        if interaction.user.mention in self.bot.mogi["voters"]:
-            return await interaction.respond("You voted already", ephemeral=True)
-        self.bot.mogi["voters"].append(interaction.user.mention)
-
-        self.bot.mogi["voters"].append(interaction.user.mention)
-        self.bot.mogi["votes"][button.label.lower()] += 1
+        vote(self.bot, button.label.lower(), interaction.user)
         await interaction.respond("Voted for 3v3", ephemeral=True)
+
+        if isDecided(self.bot):
+            await interaction.channel.send(startMogi(self.bot))
 
     @discord.ui.button(label="4v4")
     async def btn4v4(self, button: discord.ui.Button, interaction: Interaction):
-        if (
-            not (self.count % 4 == 0 and self.count > 4)
-        ) or interaction.user.mention not in self.bot.mogi["players"]:
-            return await interaction.respond("Can't vote for that", ephemeral=True)
+        if not canVote(self.bot, button.label.lower(), interaction.user.mention):
+            return await interaction.respond("Can't vote on that", ephemeral=True)
 
-        if interaction.user.mention in self.bot.mogi["voters"]:
-            return await interaction.respond("You voted already", ephemeral=True)
-        self.bot.mogi["voters"].append(interaction.user.mention)
-
-        self.bot.mogi["voters"].append(interaction.user.mention)
-        self.bot.mogi["votes"][button.label.lower()] += 1
+        vote(self.bot, button.label.lower(), interaction.user)
         await interaction.respond("Voted for 4v4", ephemeral=True)
+
+        if isDecided(self.bot):
+            await interaction.channel.send(startMogi(self.bot))
 
     @discord.ui.button(label="6v6")
     async def btn6v6(self, button: discord.ui.Button, interaction: Interaction):
-        if (
-            not (self.count % 6 == 0 and self.count > 6)
-        ) or interaction.user.mention not in self.bot.mogi["players"]:
-            return await interaction.respond("Can't vote for that", ephemeral=True)
+        if not canVote(self.bot, button.label.lower(), interaction.user.mention):
+            return await interaction.respond("Can't vote on that", ephemeral=True)
 
-        if interaction.user.mention in self.bot.mogi["voters"]:
-            return await interaction.respond("You voted already", ephemeral=True)
-        self.bot.mogi["voters"].append(interaction.user.mention)
-
-        self.bot.mogi["voters"].append(interaction.user.mention)
-        self.bot.mogi["votes"][button.label.lower()] += 1
+        vote(self.bot, button.label.lower(), interaction.user)
         await interaction.respond("Voted for 6v6", ephemeral=True)
+
+        if isDecided(self.bot):
+            await interaction.channel.send(startMogi(self.bot))
 
 
 class start(commands.Cog):
@@ -120,12 +151,14 @@ class start(commands.Cog):
         if self.bot.mogi["voting"]:
             return await ctx.respond("Already started a vote", ephemeral=True)
         if not ctx.author.mention in self.bot.mogi["players"]:
-            return await ctx.respond("You can't start a mogi you aren't in", ephemeral=True)
+            return await ctx.respond(
+                "You can't start a mogi you aren't in", ephemeral=True
+            )
         if len(self.bot.mogi["players"]) < 3:
             return await ctx.respond("Can't start a mogi with less than 3 players")
         if self.bot.mogi["running"]:
             return await ctx.respond("Mogi is already in play")
-        
+
         self.bot.mogi["voting"] = 1
         self.bot.mogi["locked"] = True
 
