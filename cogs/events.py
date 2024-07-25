b69523b061bb1dcacf922c43acd48fe97d81c5d9
probablyjassin <aouanijassin@gmail.com>
2024-07-25 13:20:01 +0200

    small changes to automated responses

--- cogs/events.py
+++ cogs/events.py
@@ -20,11 +20,13 @@ class events(commands.Cog):
         if message.author == self.bot.user:
             return
         
+        # for the funnies
         if "dk summit" in message.content.lower() and random.random() < 0.4:
-            await message.channel.send(random.choice(["DDDDKKKK SUMIIIIT", "dk summit mentioned", "best track in the game"]))
+            await message.channel.send(random.choice(["DDDDKKKK SUMIIIIT", "dk summit mentioned", "mitsiku summit"]))
         if " election" in message.content.lower() and random.random() < 0.5:
             await message.channel.send(":flag_us:")
 
+        # delete any unwanted messages in #lounge-information
         if message.channel.id == 1181312934803144724:
             if not message.author.guild_permissions.administrator or not message.author.guild_permissions.moderate_members and not message.content.startswith("/register"):
                 await message.delete()
@@ -32,8 +34,8 @@ class events(commands.Cog):
         if message.channel.type == discord.ChannelType.private :
             for hint in ["information", "piracy", "high", "seas", "sea", "yuzu", "download", "link"]:
                 if hint in message.content.lower():
-                    await message.channel.send(f"{os.getenv('YUZU_URL')}")
-                    return await message.channel.send("Please do not share this link with others so that it stays away from big N's eyes. Instead hint others at asking me here in DMs. Thank you")
+                    #await message.channel.send(f"{os.getenv('YUZU_URL')}")
+                    return await message.channel.send("The legality of sharing yuzu is unclear to us, so we can't directly hand it out to people. You will have to obtain your own copy.")
 
 def setup(bot: commands.Bot):
     bot.add_cog(events(bot))
