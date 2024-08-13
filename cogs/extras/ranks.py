13030fbc94f5de17248c1bdaff7ed9b9d99e334d
probablyjassin <aouanijassin@gmail.com>
2024-08-13 14:44:18 +0200

    ranks variable

--- cogs/extras/ranks.py
+++ cogs/extras/ranks.py
@@ -15,3 +15,13 @@ def calcRank(mmr):
         if start <= mmr <= end:
             return range_info["name"]
     return "---"
+
+ranks = [
+        {"name": "Wood", "range": (-math.inf, 1)},
+        {"name": "Bronze", "range": (2, 1499)},
+        {"name": "Silver", "range": (1400, 2999)},
+        {"name": "Gold", "range": (3000, 5099)},
+        {"name": "Platinum", "range": (5100, 6999)},
+        {"name": "Diamond", "range": (7000, 9499)},
+        {"name": "Master", "range": (9500, math.inf)},
+    ]
