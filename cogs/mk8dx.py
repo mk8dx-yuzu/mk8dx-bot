3625595ff8164a088fd428516c06ceb0b8af03fd
probablyjassin <aouanijassin@gmail.com>
2024-08-14 23:56:54 +0200

    SURELY this worky

--- cogs/mk8dx.py
+++ cogs/mk8dx.py
@@ -145,8 +145,8 @@ class mk8dx(commands.Cog):
         )
         embed.add_field(name="Discord", value=f"<@{player['discord']}>")
 
-        #if player["joined"]:
-        #    embed.add_field(name="Joined", value=f"{datetime.datetime.fromtimestamp(player['joined']).strftime('%B %d %Y')}")
+        if player.get("joined"):
+            embed.add_field(name="Joined", value=f"{datetime.datetime.fromtimestamp(player['joined']).strftime('%B %d %Y')}")
 
         rank = calcRank(player["mmr"])
         embed.add_field(name="Rank", value=f"{rank}")
