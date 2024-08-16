e1272f70ee79bd29c67dab28baf94c1614a1a830
probablyjassin <aouanijassin@gmail.com>
2024-08-16 21:44:07 +0200

    some users cant be dmd

--- cogs/purge.py
+++ cogs/purge.py
@@ -20,20 +20,23 @@ class purge(commands.Cog):
         players_with_no_mogis = list(self.players.find({"mmr": 2000, "wins": 0, "losses": 0, "history": [], "inactive": { "$exists": False }}))
         self.players.update_many({"mmr": 2000, "wins": 0, "losses": 0, "history": [], "inactive": { "$exists": False }}, {"$set": {"inactive": True}})
 
+        missed = 0
         for player in players_with_no_mogis:
-            user = await self.bot.fetch_user(int(player["discord"]))
-            await user.send(f"""
-                Hello,
-                You've registered yourself for Yuzu Online's competitive MK8DX Lounge Event as {player['name']}.
-                However, you have not yet played any events. We try to keep the ranking list clean of inactive players, so **we have marked you as 'inactive'**.
-                \n
-                If you don't want your registration deleted, simply **use the '/reactivate' slash command** here or in the server's Lounge channels. This shows us that you still want to play.
-                Otherwise, all registrations marked for deletion **will be removed from the leaderboard** and deleted after about **2 days**.
-                \n
-                Don't worry, even if this happens, you can simply re-register later in https://discord.com/channels/1084911987626094654/1181312934803144724 .
-            """)
-
-        await ctx.respond(f"Marked {len(players_with_no_mogis)} accounts as inactive and DMed users.")
+            try:
+                user = await self.bot.fetch_user(int(player["discord"]))
+                await user.send(f"""
+                    Hello,
+                    You've registered yourself for Yuzu Online's competitive MK8DX Lounge Event as {player['name']}.
+                    However, you have not yet played any events. We try to keep the ranking list clean of inactive players, so **we have marked you as 'inactive'**.
+                    \n
+                    If you don't want your registration deleted, simply **use the '/reactivate' slash command** here or in the server's Lounge channels. This shows us that you still want to play.
+                    Otherwise, all registrations marked for deletion **will be removed from the leaderboard** and deleted after about **2 days**.
+                    \n
+                    Don't worry, even if this happens, you can simply re-register later in https://discord.com/channels/1084911987626094654/1181312934803144724 .
+                """)
+            except:
+                missed += 1
+        await ctx.respond(f"Marked {len(players_with_no_mogis)} accounts as inactive and DMed {len(players_with_no_mogis)-missed} users.")
 
     @slash_command(name="reactivate", description="use this to unmark your account from being inactive if you have not played any events")
     async def reactivate(self, ctx: ApplicationContext):
