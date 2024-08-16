f177f6be100797cb794ea0d6390f4df574dbf71b
probablyjassin <aouanijassin@gmail.com>
2024-08-16 13:51:52 +0200

    small improvement

--- cogs/purge.py
+++ cogs/purge.py
@@ -15,11 +15,11 @@ class purge(commands.Cog):
     @slash_command(name="purge_leaderboard", description="flag accounts with no mogis played for deletion, dm them with an option to prevent")
     @is_admin()
     async def purge_leaderboard(self, ctx: ApplicationContext):
-        players_with_no_mogis = self.players.find({"mmr": 2000, "wins": 0, "losses": 0, "history": []})
+        players_with_no_mogis = list(self.players.find({"mmr": 2000, "wins": 0, "losses": 0, "history": []}))
         names = ""
-        for player in list(players_with_no_mogis):
+        for player in players_with_no_mogis:
             names += f"{player['name']}, "
-        await ctx.respond(f"{names[:1990]}... {len(list(players_with_no_mogis))}")
+        await ctx.respond(f"{names[:1990]}... {len(players_with_no_mogis)}")
 
         
 def setup(bot: commands.Bot):
