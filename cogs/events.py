a555a0a61390be6bdd0f526e66d52febb2c65119
probablyjassin <aouanijassin@gmail.com>
2024-08-17 21:46:50 +0200

    funny

--- cogs/events.py
+++ cogs/events.py
@@ -41,8 +41,10 @@ class events(commands.Cog):
                 await message.channel.send("having fun? get banned loser")
             
             if "my dream" in message.content.lower():
-                await message.channel.send("eyo bro, sorry that im writing u directly, U seem to have the File to Zelda totk. i want to play it on my pc but im too dumb to get it running and i dont seem to have the right game files. can u help me out? maybe by a link to the gamefile and a little guide to set that shit up. would be my dream bro")
-            
+                if 0.1 < random.random():
+                    return await message.channel.send("eyo bro, sorry that im writing u directly, U seem to have the File to Zelda totk. i want to play it on my pc but im too dumb to get it running and i dont seem to have the right game files. can u help me out? maybe by a link to the gamefile and a little guide to set that shit up. would be my dream bro")
+                await message.channel.send("Hello, recently I've downloaded a rom of tears of the kingdom. But when I launch the game with yuzu. He says me this : “The titlekey for this rights ID could not be found” I've tried to get new keys but it's the same results. Can you help me pls ? (Sorry for my English, I'm French)")
+
             if "where" in message.content.lower() and "password" in message.content.lower():
                 await message.channel.send("plz password mario kart 8 Eu main ? :3 Use Google Translate. I have a hard time understanding English, there is a server that asks me for a password and it goes to this discord, I thought I would find it here. I am from Chile (Spanish)")
             
