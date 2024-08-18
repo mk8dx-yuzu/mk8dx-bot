5aea69d01af33cea1fa9a3235f61cadb0eb13ed7
probablyjassin <aouanijassin@gmail.com>
2024-08-18 15:48:31 +0200

    reanme teams cat to team

--- cogs/mogi/tags.py
+++ cogs/mogi/tags.py
@@ -7,9 +7,9 @@ class tags(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
-    teams = SlashCommandGroup(name = "tags", description = "Edit Team tags and apply/remove respective roles")
+    team = SlashCommandGroup(name = "tags", description = "Edit Team tags and apply/remove respective roles")
 
-    @teams.command(name="tag", description="set a tag for your own team")
+    @team.command(name="tag", description="set a tag for your own team")
     async def tag(self, ctx: ApplicationContext, tag = Option(str, name="tag", description="what tag to set for your team", required=True)):
         team_i = None
         for i, subarray in enumerate(self.bot.mogi["teams"]):
@@ -19,7 +19,7 @@ class tags(commands.Cog):
             self.bot.mogi["team_tags"][int(team_i)] = tag
         await ctx.respond(f"Team {team_i+1} tag: {tag}")
 
-    @teams.command(name = "set", description = "set a tag for any team by number", guild_only=True)
+    @team.command(name = "set", description = "set a tag for any team by number", guild_only=True)
     async def set(
         self, 
         ctx: ApplicationContext, 
@@ -29,7 +29,7 @@ class tags(commands.Cog):
         self.bot.mogi["team_tags"][int(teamnumber)-1] = tag
         await ctx.respond(f"Updated Team {teamnumber}'s tag to {tag}")
 
-    @teams.command(name = "apply_roles", description = "assign team roles", guild_only=True)
+    @team.command(name = "apply_roles", description = "assign team roles", guild_only=True)
     async def apply_roles(self, ctx: ApplicationContext):
         await ctx.response.defer()
         if not self.bot.mogi["format"]:
@@ -43,7 +43,7 @@ class tags(commands.Cog):
             return await ctx.respond("Assigned team roles")
         await ctx.respond("format is ffa, not team roles assigned")
 
-    @teams.command(name="unapply_roles", guild_only=True)
+    @team.command(name="unapply_roles", guild_only=True)
     async def unapply_roles(self, ctx: ApplicationContext):
         await ctx.response.defer()
         for i in [1, 2, 3, 4, 5]:
