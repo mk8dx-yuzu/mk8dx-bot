d9b4ee7245031da136e545e6f5282a921b915fdd
probablyjassin <aouanijassin@gmail.com>
2024-08-19 19:05:07 +0200

    remove /teams

--- cogs/mogi/list.py
+++ cogs/mogi/list.py
@@ -68,26 +68,6 @@ class list(commands.Cog):
                 list += "\n"
                     
         await ctx.respond(list, allowed_mentions=discord.AllowedMentions(users=False))
-
-    @slash_command(name="teams", description="Show teams")
-    async def teams(self, ctx: ApplicationContext):
-        if not self.bot.mogi["status"]:
-            return await ctx.respond("No open mogi", ephemeral=True)
-        if not len(self.bot.mogi["teams"]):
-            return await ctx.respond("No teams decided yet", ephemeral=True)
-        lineup_str = "# Teams \n\n"
-        if self.bot.mogi["format"] == "ffa":
-            for i, player in enumerate(self.bot.mogi["players"]):
-                lineup_str += f"`{i+1}:` {get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name}\n"
-        else:
-            for i, team in enumerate(self.bot.mogi["teams"]):
-                lineup_str += f"{self.bot.mogi['team_tags'][i]}\n"
-                team = [get(ctx.guild.members, id=int(player.strip('<@!>'))).display_name for player in team]
-                for player in team:   
-                    lineup_str += f"{player} +\n"
-                lineup_str += "\n"
-                    
-        await ctx.respond(lineup_str)
         
 def setup(bot: commands.Bot):
     bot.add_cog(list(bot))
