e515664940739bbcd55868c3a3fcebb568f0d1a6
probablyjassin <aouanijassin@gmail.com>
2024-08-29 23:04:33 +0200

    await

--- cogs/mogi/join.py
+++ cogs/mogi/join.py
@@ -8,31 +8,31 @@ from pymongo import collection
 from cogs.extras.utils import is_mogi_manager
 
 async def join_player(self, ctx: ApplicationContext):
-        async with self.join_sem:
-            player = self.players.find_one({"discord": str(ctx.interaction.user.id)})
-            if not player:
-                return await ctx.respond("You are not properly registered for Lounge. Your Profile might not exist or be archived.")
+    async with self.join_sem:
+        player = self.players.find_one({"discord": str(ctx.interaction.user.id)})
+        if not player:
+            return await ctx.respond("You are not properly registered for Lounge. Your Profile might not exist or be archived.")
 
-            if not self.bot.mogi["status"]:
-                return await ctx.respond(self.bot.no_mogi)
-            
-            if self.bot.mogi["locked"]:
-                return await ctx.respond(self.bot.locked_mogi)
-            
-            if ctx.author.mention in self.bot.mogi["players"]:
-                return await ctx.respond("You are already in the mogi")
-            
-            if len(self.bot.mogi["players"]) >= self.bot.mogi["player_cap"]:
-                return await ctx.respond("The mogi is already full")
-            
-            if ctx.author.mention not in self.bot.mogi["players"]:
-                self.bot.mogi["players"].append(ctx.author.mention)
-            if get(ctx.guild.roles, name="InMogi") not in ctx.author.roles:
-                await ctx.user.add_roles(get(ctx.guild.roles, name="InMogi"))
+        if not self.bot.mogi["status"]:
+            return await ctx.respond(self.bot.no_mogi)
+        
+        if self.bot.mogi["locked"]:
+            return await ctx.respond(self.bot.locked_mogi)
+        
+        if ctx.author.mention in self.bot.mogi["players"]:
+            return await ctx.respond("You are already in the mogi")
+        
+        if len(self.bot.mogi["players"]) >= self.bot.mogi["player_cap"]:
+            return await ctx.respond("The mogi is already full")
+        
+        if ctx.author.mention not in self.bot.mogi["players"]:
+            self.bot.mogi["players"].append(ctx.author.mention)
+        if get(ctx.guild.roles, name="InMogi") not in ctx.author.roles:
+            await ctx.user.add_roles(get(ctx.guild.roles, name="InMogi"))
 
-            await ctx.respond(
-                f"{ctx.author.display_name} joined the mogi!\n{len(self.bot.mogi['players'])} players are in!"
-                )
+        await ctx.respond(
+            f"{ctx.author.display_name} joined the mogi!\n{len(self.bot.mogi['players'])} players are in!"
+            )
 
 class join(commands.Cog):
     def __init__(self, bot):
@@ -43,13 +43,13 @@ class join(commands.Cog):
 
     @slash_command(name="join", description="Join the current mogi", guild_only=True)
     async def join(self, ctx: ApplicationContext):
-        join_player(self=self, ctx=ctx)
+        await join_player(self=self, ctx=ctx)
             
     @slash_command(name="joni", description="Join the current mogi")
     async def joni(self, ctx: ApplicationContext):
         if random.random() < 0.1:
             return await ctx.respond("buddy you typoed that, its not /joni")
-        join_player(self=self, ctx=ctx)
+        await join_player(self=self, ctx=ctx)
 
     @slash_command(name="leave", description="Leave the current mogi", guild_only=True)
     async def leave(self, ctx: ApplicationContext):
