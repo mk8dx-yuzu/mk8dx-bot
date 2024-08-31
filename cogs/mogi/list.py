ff131e284c8028692b7d27646d978300408ffbae
probablyjassin <aouanijassin@gmail.com>
2024-08-31 16:55:50 +0200

    less db queries for /l mmr

--- cogs/mogi/list.py
+++ cogs/mogi/list.py
@@ -22,52 +22,55 @@ class list(commands.Cog):
         if not self.bot.mogi["players"]:
             return await ctx.respond("Current mogi: \n No players")
         
+        if context == "mmr":
+            all_inmogi_profiles = list(self.players.find({"discord": {"$in": [player.strip("<@!>") for player in self.bot.mogi["players"]]}}))
+        
         if self.bot.mogi["format"] in ["ffa", ""]:
-            list = "Current mogi:\n"
+            player_list = "Current mogi:\n"
             mmrs = []
             for index, player in enumerate(self.bot.mogi["players"]):
                 user: discord.Member = get(ctx.guild.members, id=int(player.strip("<@!>")))
                 if context == "table":
-                    list += f"{getattr(user, 'nick', None) or user.display_name} +\n\n"
+                    player_list += f"{getattr(user, 'nick', None) or user.display_name} +\n\n"
 
                 elif context == "mmr":
-                    mmr = int(self.players.find_one({'discord': player.strip('<@!>')})['mmr'])
-                    list += f"*{index+1}.* {user.display_name}: {mmr}MMR\n"
+                    mmr = [profile["mmr"] for profile in all_inmogi_profiles if profile["discord"] == player.strip("<@!>")][0]
+                    player_list += f"*{index+1}.* {user.display_name}: {mmr}MMR\n"
                     mmrs.append({"name": user.display_name, "mmr": mmr})
 
                     if index+1 == len(self.bot.mogi["players"]):
                         high_mmr = max(mmrs, key=lambda x: x['mmr'])
                         low_mmr = min(mmrs, key=lambda x: x['mmr'])
                         avg_mmr = sum(item['mmr'] for item in mmrs) // len(mmrs)
-                        list += f"\nhighest mmr: {high_mmr['name']}: {high_mmr['mmr']}\nlowest mmr: {low_mmr['name']}: {low_mmr['mmr']}\naverage mmr: {avg_mmr}\n"
+                        player_list += f"\nhighest mmr: {high_mmr['name']}: {high_mmr['mmr']}\nlowest mmr: {low_mmr['name']}: {low_mmr['mmr']}\naverage mmr: {avg_mmr}\n"
                 
                 elif context == "usernames":
-                    list += f"*{index+1}.* {user.name} \n"
+                    player_list += f"*{index+1}.* {user.name} \n"
                 else:
-                    list += f"*{index+1}.* {user.display_name}\n"
+                    player_list += f"*{index+1}.* {user.display_name}\n"
 
         else:
-            list = "# Teams \n\n"
+            player_list = "# Teams \n\n"
             for i, team in enumerate(self.bot.mogi["teams"]):
-                list += f"{'## ' if context != 'table' else ''}{self.bot.mogi['team_tags'][i] or f'Team {i+1}'}\n"
+                player_list += f"{'## ' if context != 'table' else ''}{self.bot.mogi['team_tags'][i] or f'Team {i+1}'}\n"
 
                 team = [get(ctx.guild.members, id=int(player.strip('<@!>'))) for player in team]
                 for player in team:
                     if context == "table":
-                        list += f"{getattr(player, 'nick', None) or player.display_name} +\n"
+                        player_list += f"{getattr(player, 'nick', None) or player.display_name} +\n"
 
                     elif context == "mmr":
-                        list += f"- {player.display_name}: {int(self.players.find_one({'discord': str(player.id)})['mmr'])}MMR\n"
+                        player_list += f"- {player.display_name}: {[profile["mmr"] for profile in all_inmogi_profiles if profile["discord"] == player.strip("<@!>")][0]}MMR\n"
                         
                     elif context == "usernames":
-                        list += f"- {player.name} \n"
+                        player_list += f"- {player.name} \n"
 
                     else:
-                        list += f"- {player.display_name}\n"
+                        player_list += f"- {player.display_name}\n"
 
-                list += "\n"
+                player_list += "\n"
                     
-        await ctx.respond(list, allowed_mentions=discord.AllowedMentions(users=False))
+        await ctx.respond(player_list, allowed_mentions=discord.AllowedMentions(users=False))
         
 def setup(bot: commands.Bot):
     bot.add_cog(list(bot))
