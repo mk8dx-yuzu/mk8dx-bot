51bc2ed1c08a5557b106413d2b5470591c962496
probablyjassin <aouanijassin@gmail.com>
2024-09-01 21:55:11 +0200

    stop having fun (not abuse)

--- cogs/funnies.py
+++ cogs/funnies.py
@@ -23,33 +23,33 @@ class funnies(commands.Cog):
 
         if message.channel.id == 1180622895316209664:
             if "dk summit" in message.content.lower() and random.random() < 0.4:
-                await message.channel.send(random.choice(["DDDDKKKK SUMIIIIT", "dk summit mentioned", "mitsiku summit"]))
+                return await message.channel.send(random.choice(["DDDDKKKK SUMIIIIT", "dk summit mentioned", "mitsiku summit"]))
 
             if (" election" in message.content.lower() or message.content.lower().startswith("election")) and random.random() < 0.5:
-                await message.channel.send(":flag_us:")
+                return await message.channel.send(":flag_us:")
 
             if self.bot.user.mention in message.content.lower() and random.random() < 0.4:
-                await message.channel.send("Grr stop pinging me")
+                return await message.channel.send("Grr stop pinging me")
 
             funnylist = ["is mogi over", "you might lose mmr", "if i sub will i lose mmr", "we don't need a sub", "what are your pc specs", "wanna 1v1"]
             if any(word in message.content.lower() for word in funnylist):
-                await message.channel.send(random.choice(funnylist))
+                return await message.channel.send(random.choice(funnylist))
 
             if "banned" in message.content.lower():
-                await message.channel.send("having fun? get banned loser")
+                return await message.channel.send("having fun? get banned loser")
 
             if "my dream" in message.content.lower():
                 if 0.1 < random.random():
                     return await message.channel.send("eyo bro, sorry that im writing u directly, U seem to have the File to Zelda totk. i want to play it on my pc but im too dumb to get it running and i dont seem to have the right game files. can u help me out? maybe by a link to the gamefile and a little guide to set that shit up. would be my dream bro")
-                await message.channel.send("Hello, recently I've downloaded a rom of tears of the kingdom. But when I launch the game with yuzu. He says me this : “The titlekey for this rights ID could not be found” I've tried to get new keys but it's the same results. Can you help me pls ? (Sorry for my English, I'm French)")
+                return await message.channel.send("Hello, recently I've downloaded a rom of tears of the kingdom. But when I launch the game with yuzu. He says me this : “The titlekey for this rights ID could not be found” I've tried to get new keys but it's the same results. Can you help me pls ? (Sorry for my English, I'm French)")
 
             if "where" in message.content.lower() and "password" in message.content.lower():
-                await message.channel.send("plz password mario kart 8 Eu main ? :3 Use Google Translate. I have a hard time understanding English, there is a server that asks me for a password and it goes to this discord, I thought I would find it here. I am from Chile (Spanish)")
+                return await message.channel.send("plz password mario kart 8 Eu main ? :3 Use Google Translate. I have a hard time understanding English, there is a server that asks me for a password and it goes to this discord, I thought I would find it here. I am from Chile (Spanish)")
 
             if "coconut" in message.content.lower():
                 with open("media/coconut.png", "rb") as f:
                     media = discord.File(f)
-                    await message.channel.send(file=media)
+                    return await message.channel.send(file=media)
 
         if message.content == "dc":
             await message.channel.send("https://autocompressor.net/av1?s=wfinqwL9")
